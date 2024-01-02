0eba315416fa781a40f3d1970ed24ae550866e38
uuo00_n <huangjunbo1107@outlook.com>
2024-01-02 14:07:19 +0800

    画板类执行完成

--- src/flyingbrid/BirdGame.java
+++ src/flyingbrid/BirdGame.java
@@ -1,4 +1,33 @@
 package flyingbrid;
 
-public class BirdGame {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * 画板类(游戏操作类)
+ */
+public class BirdGame extends JPanel {
+    //    绘制背景图片(BufferedImage图片缓冲区)
+    public BufferedImage bg;
+
+    //    赋值
+    public BirdGame() {
+        try {
+//            异常：程序在运行时发生不可控的事件
+//            getClass().getResource() 获取资源路径
+            bg = ImageIO.read(getClass().getResource("./img/bg.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("图片未找到！");
+        }
+    }
+
+    //    画画(Graphics 画笔)
+    public void paint(Graphics g) {
+        g.drawImage(bg, 0, 0, null);
+
+    }
 }
--- src/flyingbrid/StartGame.java
+++ src/flyingbrid/StartGame.java
@@ -5,18 +5,24 @@ import javax.swing.*;
 /**
  * 游戏入口
  * javax.swing 图形界面设计技术
- * */
+ */
 public class StartGame {
     public static void main(String[] args) {
 //        绘制游戏窗口
         JFrame frame = new JFrame("飞扬的小鸟");
 //        设置窗口大小
-        frame.setSize(600,970);
+        frame.setSize(432, 644);
+//        创建画板对象
+        BirdGame game = new BirdGame();
+//        添加画板
+        frame.add(game);
 //        布局样式---居中
         frame.setLocationRelativeTo(null);
 //        添加可视化窗口
         frame.setVisible(true);
 //        关闭应用程序
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
     }
 }
